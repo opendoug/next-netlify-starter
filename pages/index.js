de0bde0d3dbe4872a542d1bc3e6cc030bce50f19
Doug <doug@waltersworks.com>
2023-02-27 22:45:34 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,9 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <p className="banzai">
+          I'm not sure what to do with this, but I'll figure something out...
+        </p>
       </main>
 
       <Footer />
